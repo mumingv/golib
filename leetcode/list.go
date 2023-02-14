8f453db97e15699ec7f3e1ae03271447592b1f58
Yin Jie <mumingv@163.com>
2023-02-14 13:18:41 +0800

    新增链表结构

---
+++ leetcode/list.go
@@ -0,0 +1,43 @@
+package leetcode
+
+import "fmt"
+
+/**
+ * Definition for singly-linked list.
+ */
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func NewList(a []int) *ListNode {
+	var head, p *ListNode = nil, nil
+	for _, v := range a {
+		temp := new(ListNode)
+		temp.Val = v
+		if head == nil {
+			head = temp
+			p = temp
+		} else {
+			p.Next = temp
+			p = p.Next
+		}
+	}
+	return head
+}
+
+func (l *ListNode) Len() int {
+	lenth := 0
+	for p := l; p != nil; p = p.Next {
+		lenth++
+	}
+	return lenth
+}
+
+func (l *ListNode) Print() {
+	slice := []int{}
+	for p := l; p != nil; p = p.Next {
+		slice = append(slice, p.Val)
+	}
+	fmt.Println(slice)
+}
